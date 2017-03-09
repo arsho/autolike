e61f0f06fb31aa7b1f4ec54fe190276cc960ad42
Ahmedur Rahman Shovon <shovon.sylhet@gmail.com>
2017-03-09 12:50:08 +0600

    First Commit

---
+++ README.rst
@@ -0,0 +1,28 @@
+AutoLike
+--------
+Automatically like any given Facebook URL if the user is logged in.
+The facebook(url, run_time) method takes a Facebook URL and run time in seconds.
+It will return a dictionary with status, message, url, like and time attribute.
+If the status has "success" value then the program is successfully completed.
+
+DO NOT MISUSE IT FOR DISTURBING INDIVIDUALS.
+
+The current version is tested on Windows(10) and Ubuntu(16.04 LTS).
+
+To install (in Ubuntu having both Python2 and Python3 by default)::
+
+    >>> sudo pip3 install autolike
+
+To install (in Windows with which has only Python 3 installed)::
+
+    >>> sudo pip install autolike
+
+To use (with caution), simply do::
+
+    >>> import autolike
+	>>> url = "https://www.facebook.com/" 			#any Facebook URL 
+	>>> run_time = 200	
+    >>> like_result_dict = autolike.facebook(url, run_time)
+
+BEFORE RUNNING THE PROGRAM, YOU NEED TO BE LOGGED IN TO FACEBOOK USING YOUR DEFAULT BROWSER.
+	
---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+setup(name='autolike',
+      version='0.0.1',
+      description='Automatically like any given Facebook URL if the user is logged in.',
+      long_description=readme(),
+      install_requires=['pymsgbox', 'PyTweening>=1.0.1', 'Pillow', 'pyscreeze', 'pyautogui'],
+      classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License'
+      ],
+      keywords='Facebook fb like liker autolike autoliker automation',
+      url='http://github.com/arsho/like',
+      author='Ahmedur Rahman Shovon',
+      author_email='shovon.sylhet@gmail.com',
+      license='BSD',
+      packages=['autolike'],
+      package_data={'autolike': ['autolike/*.png']},
+      include_package_data=True,
+      zip_safe=False)
